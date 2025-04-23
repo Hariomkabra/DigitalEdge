6872ca5f4af70817424bd517bb429b8a0bb28ea7
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2025-04-23 11:02:58 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Ensures assets are served correctly in production
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
 });
+
